d41b70cdb6639fd5ff8f03cf87775a4f0f4e1cdc
Jescklay Matos <jescklaysoft@hotmail.com>
2024-06-14 16:29:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function clickMenu() {
+  document.getElementById("burguer").classList.toggle("bi-x");
+  document.getElementById("burguer").classList.toggle("bi-list");
+}
+
+function clickMenu() {
+  if (itens.style.display == "block") {
+    itens.style.display = "none";
+  } else {
+    itens.style.display = "block";
+  }
+}
